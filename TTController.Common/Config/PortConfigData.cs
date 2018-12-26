52c97221f903961f226c4470ef844546a7e3c5fd
Unknown <kamil.pietka@yahoo.pl>
2018-12-26 20:13:05 +0100

    Update PortConfigData

--- TTController.Common/Config/PortConfigData.cs
+++ TTController.Common/Config/PortConfigData.cs
@@ -2,6 +2,13 @@
 {
     public class PortConfigData
     {
+        public PortIdentifier Port { set; get; }
         public string Name { set; get; }
+        public int LedRotation { set; get; }
+
+        public PortConfigData(PortIdentifier port)
+        {
+            Port = port;
+        }
     }
 }
