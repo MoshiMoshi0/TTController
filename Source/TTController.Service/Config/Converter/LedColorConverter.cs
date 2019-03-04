175b75a47c47c4a7103ee0b77a2e5b17e806dcf4
Unknown <kamil.pietka@yahoo.pl>
2019-03-04 21:20:45 +0100

    Split converters to separate files

---
+++ Source/TTController.Service/Config/Converter/LedColorConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using TTController.Common;
+
+namespace TTController.Service.Config.Converter
+{
+    class LedColorConverter : ObjectToArrayConverter<LedColor>
+    {
+        protected override object[] CreateConstructorArgs(JArray array) =>
+            new object[] { array[0].Value<byte>(), array[1].Value<byte>(), array[2].Value<byte>() };
+    }
+}
---
+++ Source/TTController.Service/Config/Converter/PortIdentifierConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TTController.Common;
+
+namespace TTController.Service.Config.Converter
+{
+    public class PortIdentifierConverter : ObjectToArrayConverter<PortIdentifier>
+    {
+        protected override object[] CreateConstructorArgs(JArray array) =>
+            new object[] { array[0].Value<int>(), array[1].Value<int>(), array.Count == 2 ? (byte)0 : array[2].Value<byte>() };
+
+        protected override IEnumerable<PropertyInfo> FilterProperties(PortIdentifier value)
+        {
+            foreach (var property in value.GetType().GetProperties())
+            {
+                if (string.CompareOrdinal(property.Name, nameof(PortIdentifier.Id)) != 0 ||
+                    (byte)property.GetValue(value) > 0)
+                    yield return property;
+            }
+        }
+    }
+
+}
