d55e2e034cbc2d671080d178c3e92b57674d62fb
Unknown <kamil.pietka@yahoo.pl>
2019-03-06 20:14:17 +0100

    Add WaveEffect

---
+++ Plugins/Effects/TTController.Plugin.WaveEffect/WaveEffect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TTController.Common;
+
+namespace TTController.Plugin.WaveEffect
+{
+    public class WaveEffectConfig : EffectConfigBase
+    {
+        public List<LedColor> Colors { get; set; }
+        public int TickInterval { get; set; }
+    }
+
+    public class WaveEffect : EffectBase<WaveEffectConfig>
+    {
+        private int _tick;
+        private readonly LinkedList<LedColor> _colors;
+
+        public WaveEffect(WaveEffectConfig config) : base(config)
+        {
+            _tick = Config.TickInterval;
+            _colors = new LinkedList<LedColor>(Config.Colors);
+        }
+
+        public override byte EffectByte => (byte)EffectType.ByLed;
+
+        public override IDictionary<PortIdentifier, List<LedColor>> GenerateColors(List<PortIdentifier> ports, ICacheProvider cache)
+        {
+            if (_tick++ >= Config.TickInterval)
+            {
+                _tick = 0;
+                _colors.AddFirst(_colors.Last.Value);
+                _colors.RemoveLast();
+            }
+
+            return ports.ToDictionary(p => p, p => _colors.ToList());
+        }
+    }
+}
