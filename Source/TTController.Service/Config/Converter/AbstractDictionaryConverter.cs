82c5a1ad91768b41bae922e647fea509ab1d7fc4
Unknown <kamil.pietka@yahoo.pl>
2019-05-07 01:14:57 +0200

    Cleanup AbstractDictionaryConverter

--- Source/TTController.Service/Config/Converter/AbstractDictionaryConverter.cs
+++ Source/TTController.Service/Config/Converter/AbstractDictionaryConverter.cs
@@ -11,19 +11,38 @@ namespace TTController.Service.Config.Converter
     {
         public override void WriteJson(JsonWriter writer, IDictionary<TKey, TValue> value, JsonSerializer serializer)
         {
-            var o = new JObject();
+            var o = new JArray();
             foreach (var (k, v) in value)
-                o.Add(WriteKey(k), WriteValue(v));
+            {
+                o.Add(new JObject
+                {
+                    { KeyName(), WriteKey(k) },
+                    { ValueName(), WriteValue(v) }
+                });
+            }
             o.WriteTo(writer);
         }
 
         public override IDictionary<TKey, TValue> ReadJson(JsonReader reader, Type objectType, IDictionary<TKey, TValue> existingValue, bool hasExistingValue,
-            JsonSerializer serializer) =>
-            JObject.Load(reader).Properties().ToDictionary(ReadKey, ReadValue);
+            JsonSerializer serializer)
+        {
+            var result = new Dictionary<TKey, TValue>();
+
+            foreach (JObject o in JArray.Load(reader))
+            {
+                var key = ReadKey(o[KeyName()] as JProperty);
+                var value = ReadValue(o[ValueName()] as JProperty);
+                result.Add(key, value);
+            }
+
+            return result;
+        }
 
         protected abstract TKey ReadKey(JProperty property);
         protected abstract TValue ReadValue(JProperty property);
-        protected abstract string WriteKey(TKey key);
+        protected abstract JToken WriteKey(TKey key);
         protected abstract JToken WriteValue(TValue value);
+        protected abstract string KeyName();
+        protected abstract string ValueName();
     }
 }
