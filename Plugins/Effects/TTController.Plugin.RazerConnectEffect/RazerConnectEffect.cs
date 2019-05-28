3316c5f2ac5006cb6a054640d0068cc21a1f623f
Unknown <kamil.pietka@yahoo.pl>
2019-05-28 16:29:00 +0200

    Add RazerConnectEffect

---
+++ Plugins/Effects/TTController.Plugin.RazerConnectEffect/RazerConnectEffect.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using TTController.Common;
+using TTController.Common.Plugin;
+
+namespace TTController.Plugin.RazerConnectEffect
+{
+    public class RazerConnectConfig : EffectConfigBase { }
+
+    public class RazerConnectEffect : EffectBase<RazerConnectConfig>
+    {
+        private readonly int BroadcastColorCount = 5;
+
+        private readonly LedColor[] _colors;
+        private readonly int[] _packedColors;
+
+        private bool _connected;
+
+        public override string EffectType => "ByLed";
+        public override bool IsEnabled(ICacheProvider cache) => base.IsEnabled(cache) && _connected;
+
+        public RazerConnectEffect(RazerConnectConfig config) : base(config)
+        {
+            _connected = false;
+            _colors = new LedColor[BroadcastColorCount];
+            _packedColors = new int[BroadcastColorCount];
+
+            RzChromaBroadcastNative.Load();
+            var init = RzChromaBroadcastNative.Init(Guid.Parse("b0ecdaf9-26b2-d33f-f046-1c44ce64eb58"));
+            if (init == RzResult.SUCCESS)
+            {
+                var register = RzChromaBroadcastNative.RegisterEventNotification(BroadcastCallback);
+                if (register == RzResult.SUCCESS)
+                    _connected = true;
+            }
+        }
+
+        private int BroadcastCallback(int message, IntPtr data)
+        {
+            if (message == 1)
+            {
+                if (data != IntPtr.Zero)
+                {
+                    Marshal.Copy(data, _packedColors, 0, BroadcastColorCount);
+
+                    for (var i = 0; i < BroadcastColorCount; i++)
+                    {
+                        _colors[i].R = (byte)((_packedColors[i] >> 0) & 0xf);
+                        _colors[i].G = (byte)((_packedColors[i] >> 8) & 0xff);
+                        _colors[i].B = (byte)((_packedColors[i] >> 16) & 0xff);
+                    }
+                }
+            }
+            else if (message == 2)
+            {
+                if (data.ToInt32() == 1)
+                    _connected = true;
+                else if (data.ToInt32() == 2)
+                    _connected = false;
+            }
+
+            return 0;
+        }
+
+        public override IDictionary<PortIdentifier, List<LedColor>> GenerateColors(List<PortIdentifier> ports, ICacheProvider cache)
+        {
+            return ports.ToDictionary(p => p, _ => _colors.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            var unRegister = RzChromaBroadcastNative.UnregisterEventNotification();
+            var unInit = RzChromaBroadcastNative.UnInit();
+            RzChromaBroadcastNative.UnLoad();
+        }
+    }
+}
---
+++ Plugins/Effects/TTController.Plugin.RazerConnectEffect/RzChromaBroadcastNative.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTController.Plugin.RazerConnectEffect
+{
+    public enum RzResult
+    {
+        SUCCESS = 0
+    }
+
+    public static class RzChromaBroadcastNative
+    {
+        private static IntPtr _dllHandle;
+
+        public static void Load()
+        {
+            if (_dllHandle != IntPtr.Zero)
+                return;
+
+            _dllHandle = LoadLibrary("RzChromaBroadcastAPI.dll");
+            _initPointer = (InitPointer)Marshal.GetDelegateForFunctionPointer(GetProcAddress(_dllHandle, "Init"), typeof(InitPointer));
+            _unInitPointer = (UnInitPointer)Marshal.GetDelegateForFunctionPointer(GetProcAddress(_dllHandle, "UnInit"), typeof(UnInitPointer));
+            _registerEventNotificationPointer = (RegisterEventNotificationPointer)Marshal.GetDelegateForFunctionPointer(GetProcAddress(_dllHandle, "RegisterEventNotification"), typeof(RegisterEventNotificationPointer));
+            _unRegisterEventNotificationPointer = (UnRegisterEventNotificationPointer)Marshal.GetDelegateForFunctionPointer(GetProcAddress(_dllHandle, "UnRegisterEventNotification"), typeof(UnRegisterEventNotificationPointer));
+        }
+
+        public static void UnLoad()
+        {
+            while (FreeLibrary(_dllHandle)) ;
+            _dllHandle = IntPtr.Zero;
+        }
+
+        private static InitPointer _initPointer;
+        private static UnInitPointer _unInitPointer;
+        private static RegisterEventNotificationPointer _registerEventNotificationPointer;
+        private static UnRegisterEventNotificationPointer _unRegisterEventNotificationPointer;
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate RzResult InitPointer(uint a, uint b, uint c, uint d);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate RzResult UnInitPointer();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate RzResult RegisterEventNotificationPointer(RzBroadcastCallback callback);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate RzResult UnRegisterEventNotificationPointer();
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate int RzBroadcastCallback(int message, IntPtr data);
+
+        internal static RzResult Init(Guid guid)
+        {
+            var i = new BigInteger(guid.ToByteArray());
+
+            var a = (uint)((i >> 96) & 0xFFFFFFFF);
+            var b = (uint)((i >> 64) & 0xFFFFFFFF);
+            var c = (uint)((i >> 32) & 0xFFFFFFFF);
+            var d = (uint)((i >> 0) & 0xFFFFFFFF);
+
+            return _initPointer(a, b, c, d);
+        }
+        internal static RzResult UnInit() => _unInitPointer();
+
+        internal static RzResult RegisterEventNotification(RzBroadcastCallback callback) => _registerEventNotificationPointer(callback);
+        internal static RzResult UnregisterEventNotification() => _unRegisterEventNotificationPointer();
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string dllToLoad);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool FreeLibrary(IntPtr dllHandle);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetProcAddress(IntPtr dllHandle, string name);
+    }
+}
