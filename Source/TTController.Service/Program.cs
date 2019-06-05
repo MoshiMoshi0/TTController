62f4bf162159a6def7228dfbf67d52e1d28ccf81
Unknown <kamil.pietka@yahoo.pl>
2019-06-05 19:47:44 +0200

    Refactor "Show hardware info" into "Debug" menu

--- Source/TTController.Service/Program.cs
+++ Source/TTController.Service/Program.cs
@@ -24,32 +24,7 @@ namespace TTController.Service
         {
             if (Environment.UserInteractive)
             {
-                var menu = new MenuPage("Main Menu");
-                menu.Add("Manage Service", ManageService, () => true);
-                menu.Add("Run in console", () => {
-                    Console.Clear();
-                    Console.ResetColor();
-                    var service = new TTService();
-                    service.Initialize();
-                    Console.ReadKey(true);
-                    service.Finalize(ComputerStateType.Shutdown);
-                    Console.WriteLine("Press any key to return to the menu...");
-                    Console.ReadKey(true);
-                    return false;
-                }, () => Service == null || Service.Status != ServiceControllerStatus.Running);
-                menu.Add("Show hardware info", () => {
-                    ShowHardwareInfo();
-                    return false;
-                }, () => Service != null && Service.Status != ServiceControllerStatus.Running);
-                menu.Add("Exit", () => true, () => true, '0');
-
-                while (true)
-                {
-                    Console.Clear();
-                    var selected = menu.Show();
-                    if (selected.Callback())
-                        return;
-                }
+                MainMenu();
             }
             else
             {
@@ -57,7 +32,34 @@ namespace TTController.Service
             }
         }
 
-        private static bool ManageService()
+        private static void MainMenu()
+        {
+            var menu = new MenuPage("Main Menu");
+            menu.Add("Manage Service", ManageServiceMenu, () => true);
+            menu.Add("Run in console", () => {
+                Console.Clear();
+                Console.ResetColor();
+                var service = new TTService();
+                service.Initialize();
+                Console.ReadKey(true);
+                service.Finalize(ComputerStateType.Shutdown);
+                Console.WriteLine("Press any key to return to the menu...");
+                Console.ReadKey(true);
+                return false;
+            }, () => Service == null || Service.Status != ServiceControllerStatus.Running);
+            menu.Add("Debug", DebugMenu, () => Service != null && Service.Status != ServiceControllerStatus.Running);
+            menu.Add("Exit", () => true, () => true, '0');
+
+            while (true)
+            {
+                Console.Clear();
+                var selected = menu.Show();
+                if (selected.Callback())
+                    return;
+            }
+        }
+
+        private static bool ManageServiceMenu()
         {
             void StopService()
             {
@@ -145,9 +147,10 @@ namespace TTController.Service
             }
         }
 
-        private static void ShowHardwareInfo()
+        private static bool DebugMenu()
         {
-            LogManager.DisableLogging();
+            if (!LogManager.Configuration.LoggingRules.Any(r => r.IsLoggingEnabledForLevel(LogLevel.Trace)))
+                LogManager.DisableLogging();
 
             void WriteHeader(string header)
             {
@@ -163,64 +166,148 @@ namespace TTController.Service
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("-------------------------------");
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Press any key to continue...");
                 Console.ResetColor();
             }
 
-            Console.Clear();
-            WriteHeader("Controllers");
-
-            PluginLoader.Load($@"{AppDomain.CurrentDomain.BaseDirectory}\Plugins", typeof(IControllerDefinition));
-            using (var deviceManager = new DeviceManager())
+            void WaitForInput()
             {
-                foreach (var controller in deviceManager.Controllers)
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+
+            void ListControllers()
+            {
+                WriteHeader("Controllers");
+
+                PluginLoader.Load($@"{AppDomain.CurrentDomain.BaseDirectory}\Plugins", typeof(IControllerDefinition));
+                using (var deviceManager = new DeviceManager())
                 {
-                    Console.WriteLine($"Name: {controller.Name}" +
-                                      $"\nVendorId: {controller.VendorId}" +
-                                      $"\nProductId: {controller.ProductId}");
-                    Console.WriteLine($"Ports:");
-                    foreach (var port in controller.Ports)
+                    foreach (var controller in deviceManager.Controllers)
                     {
-                        var data = controller.GetPortData(port.Id);
-                        Console.WriteLine($"\tId: {port.Id}" +
-                                          $"\n\tData: {data}" +
-                                          $"\n\tIdentifier: {port}" +
-                                          $"\n");
+                        Console.WriteLine($"Name: {controller.Name}" +
+                                          $"\nVendorId: {controller.VendorId}" +
+                                          $"\nProductId: {controller.ProductId}");
+                        Console.WriteLine($"Ports:");
+                        foreach (var port in controller.Ports)
+                        {
+                            var data = controller.GetPortData(port.Id);
+                            Console.WriteLine($"\tId: {port.Id}" +
+                                              $"\n\tData: {data}" +
+                                              $"\n\tIdentifier: {port}" +
+                                              $"\n");
+                        }
+
+                        Console.WriteLine($"Available effect types:");
+                        Console.WriteLine($"{string.Join(", ", controller.EffectTypes)}");
+                        Console.WriteLine();
+                    }
+                }
+
+                WriteFooter();
+            }
+
+            void ListSensors(params SensorType[] types)
+            {
+                WriteHeader("Sensors");
+
+                string FormatValue(SensorType type, float value)
+                {
+                    switch (type)
+                    {
+                        case SensorType.Voltage:     return $"{value:F2} V";
+                        case SensorType.Clock:       return $"{value:F0} MHz";
+                        case SensorType.Load:        return $"{value:F1} %";
+                        case SensorType.Temperature: return $"{value:F1} °C";
+                        case SensorType.Fan:         return $"{value:F0} RPM";
+                        case SensorType.Flow:        return $"{value:F0} L/h";
+                        case SensorType.Control:     return $"{value:F1} %";
+                        case SensorType.Level:       return $"{value:F1} %";
+                        case SensorType.Power:       return $"{value:F0} W";
+                        case SensorType.Data:        return $"{value:F0} GB";
+                        case SensorType.SmallData:   return $"{value:F1} MB";
+                        case SensorType.Factor:      return $"{value:F3}";
+                        case SensorType.Frequency:   return $"{value:F1} Hz";
+                        case SensorType.Throughput:  return $"{value:F1} B/s";
                     }
 
-                    Console.WriteLine($"Available effect types:");
-                    Console.WriteLine($"{string.Join(", ", controller.EffectTypes)}");
-                    Console.WriteLine();
+                    return $"{value}";
                 }
-            }
 
-            WriteHeader("Plugins");
-
-            var pluginAssemblies = PluginLoader.SearchAll($@"{AppDomain.CurrentDomain.BaseDirectory}\Plugins");
-            Console.WriteLine("Valid plugins:");
-            foreach (var assembly in pluginAssemblies)
-                Console.WriteLine($"\t{Path.GetFileName(assembly.Location)}");
-
-            Console.WriteLine();
-
-            WriteHeader("Sensors");
-            using (var _openHardwareMonitorFacade = new OpenHardwareMonitorFacade())
-            {
-                var availableSensors = _openHardwareMonitorFacade.Sensors.Where(s => s.SensorType == SensorType.Temperature);
-                foreach (var (hardware, sensors) in availableSensors.GroupBy(s => s.Hardware))
+                using (var _openHardwareMonitorFacade = new OpenHardwareMonitorFacade())
                 {
-                    Console.WriteLine($"{hardware.Name}:");
-                    hardware.Update();
+                    var availableSensors = _openHardwareMonitorFacade.Sensors.Where(s => types.Length > 0 ? types.Contains(s.SensorType) : true);
+                    foreach (var (hardware, sensors) in availableSensors.GroupBy(s => s.Hardware))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"{hardware.Name}:");
+                        hardware.Update();
 
-                    foreach(var sensor in sensors)
-                        Console.WriteLine($"\t{sensor.Name} ({sensor.Identifier}): {sensor.Value ?? float.NaN}");
+                        foreach (var (type, group) in sensors.GroupBy(s => s.SensorType))
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine($"\t{type}");
+                            Console.ResetColor();
+                            foreach (var sensor in group)
+                                Console.WriteLine($"\t\t{sensor.Name} ({sensor.Identifier}): {FormatValue(type, sensor.Value ?? float.NaN)}");
+                        }
+                    }
                 }
+
+                WriteFooter();
             }
 
-            WriteFooter();
-            Console.ReadKey(true);
+            void ListPlugins()
+            {
+                WriteHeader("Plugins");
 
-            LogManager.EnableLogging();
+                var pluginAssemblies = PluginLoader.SearchAll($@"{AppDomain.CurrentDomain.BaseDirectory}\Plugins");
+                Console.WriteLine("Valid plugins:");
+                foreach (var assembly in pluginAssemblies)
+                    Console.WriteLine($"\t{Path.GetFileName(assembly.Location)}");
+
+                WriteFooter();
+            }
+
+            var enabled = Service != null && Service.Status != ServiceControllerStatus.Running;
+            var menu = new MenuPage("Main Menu > Debug");
+            menu.Add("Controllers", () => {
+                Console.Clear();
+                ListControllers();
+                WaitForInput();
+                return false;
+            }, () => enabled);
+            menu.Add("Sensors", () => {
+                Console.Clear();
+                ListSensors();
+                WaitForInput();
+                return false;
+            }, () => enabled);
+            menu.Add("Plugins", () => {
+                Console.Clear();
+                ListPlugins();
+                WaitForInput();
+                return false;
+            }, () => enabled);
+            menu.Add("Report", () => {
+                Console.Clear();
+                ListControllers();
+                ListSensors();
+                ListPlugins();
+                WaitForInput();
+                return false;
+            }, () => enabled);
+            menu.Add("Back", () => {
+                LogManager.EnableLogging();
+                return true;
+            }, () => true, '0');
+
+            while (true)
+            {
+                Console.Clear();
+                var selected = menu.Show();
+                if (selected.Callback())
+                    return false;
+            }
         }
 
         #region Menu
