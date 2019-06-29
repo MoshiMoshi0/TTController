fd1918650c63ad96519a4571ae81d3575f815ed0
Unknown <kamil.pietka@yahoo.pl>
2019-06-29 20:21:43 +0200

    Fix color format when sending colors to Riing controller

--- Plugins/Devices/TTController.Plugin.RiingController/RiingControllerProxy.cs
+++ Plugins/Devices/TTController.Plugin.RiingController/RiingControllerProxy.cs
@@ -14,6 +14,19 @@ namespace TTController.Plugin.RiingController
             : base(device, definition)
         { }
 
+        public override bool SetRgb(byte port, byte mode, IEnumerable<LedColor> colors)
+        {
+            var bytes = new List<byte> { 0x32, 0x52, port, mode };
+            foreach (var color in colors)
+            {
+                bytes.Add(color.R);
+                bytes.Add(color.G);
+                bytes.Add(color.B);
+            }
+
+            return Device.WriteReadBytes(bytes)?[3] == 0xfc;
+        }
+
         public override bool SetSpeed(byte port, byte speed) =>
             Device.WriteReadBytes(0x32, 0x51, port, 0x03, speed)?[3] == 0xfc;
 
