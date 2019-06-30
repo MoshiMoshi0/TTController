722205e03760f99bcdf4565f25f1b25610b21041
Unknown <kamil.pietka@yahoo.pl>
2019-06-30 12:43:06 +0200

    Fix SetRgb for Riing Duo fans

--- Plugins/Devices/TTController.Plugin.RiingTrioController/RiingTrioControllerProxy.cs
+++ Plugins/Devices/TTController.Plugin.RiingTrioController/RiingTrioControllerProxy.cs
@@ -13,25 +13,26 @@ namespace TTController.Plugin.RiingTrioController
         public override bool SetRgb(byte port, byte mode, IEnumerable<LedColor> colors)
         {
             var colorCount = colors.Count();
-            if (colorCount <= 20)
+            if (colorCount <= 12)
                 return base.SetRgb(port, mode, colors);
 
-            var result = true;
-            const byte maxPerChunk = 19;
-            for (byte chunkId = 1, chunkOffset = 0;
-                chunkOffset < colorCount;
-                chunkId++, chunkOffset += maxPerChunk)
+            bool WriteChunk(byte chunkId)
             {
+                const byte maxPerChunk = 19;
                 var bytes = new List<byte> { 0x32, 0x52, port, 0x24, 0x03, chunkId, 0x00 };
-                foreach (var color in colors.Skip(chunkOffset).Take(maxPerChunk))
+                foreach (var color in colors.Skip((chunkId - 1) * maxPerChunk).Take(maxPerChunk))
                 {
                     bytes.Add(color.G);
                     bytes.Add(color.R);
                     bytes.Add(color.B);
                 }
 
-                result &= Device.WriteReadBytes(bytes)?[3] == 0xfc;
+                return Device.WriteReadBytes(bytes)?[3] == 0xfc;
             }
+
+            var result = true;
+            for(byte i = 0x01; i <= 0x02; i++)
+                result &= WriteChunk(i);
 
             return result;
         }
