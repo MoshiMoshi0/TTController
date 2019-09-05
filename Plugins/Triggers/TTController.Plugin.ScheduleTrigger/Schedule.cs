7e5f95b3df3909838a927db0101fe43e68b1bb6e
Unknown <kamil.pietka@yahoo.pl>
2019-09-05 14:12:07 +0200

    Add ScheduleTrigger

---
+++ Plugins/Triggers/TTController.Plugin.ScheduleTrigger/Schedule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTController.Plugin.ScheduleTrigger
+{
+    public class Schedule
+    {
+        public IReadOnlyList<(TimeSpan Start, TimeSpan End)> Entries { get; }
+
+        public Schedule(IEnumerable<(TimeSpan Start, TimeSpan End)> entries)
+        {
+            Entries = entries.ToList();
+        }
+
+        public bool Contains(TimeSpan time)
+        {
+            foreach (var entry in Entries)
+                if (time >= entry.Start && time <= entry.End)
+                    return true;
+
+            return false;
+        }
+    }
+}
---
+++ Plugins/Triggers/TTController.Plugin.ScheduleTrigger/ScheduleConverter.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTController.Plugin.ScheduleTrigger
+{
+    class ScheduleConverter : JsonConverter<Schedule>
+    {
+        private readonly string Separator = " -> ";
+
+        public override void WriteJson(JsonWriter writer, Schedule value, JsonSerializer serializer)
+        {
+            var array = new JArray();
+            foreach (var entry in value.Entries)
+                array.Add($"{entry.Start:c}{Separator}{entry.End:c}");
+
+            serializer.Serialize(writer, array);
+        }
+
+        public override Schedule ReadJson(JsonReader reader, Type objectType, Schedule existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            var entries = new List<(TimeSpan Start, TimeSpan End)>();
+
+            var array = JArray.Load(reader);
+            foreach(var s in array.Values<string>())
+            {
+                var parts = s.Split(new string[] { Separator }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length != 2)
+                    continue;
+
+                if (TimeSpan.TryParse(parts[0], out var start) && TimeSpan.TryParse(parts[1], out var end))
+                    entries.Add((start, end));
+            }
+
+            return new Schedule(entries);
+        }
+    }
+}
