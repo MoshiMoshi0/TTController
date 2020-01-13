cd9a20ae53a74c89e534fb7722f073ce62060a94
Unknown <kamil.pietka@yahoo.pl>
2020-01-13 20:09:29 +0100

    Add on/off colors and full color config to BlinkEffect

--- Plugins/Effects/TTController.Plugin.BlinkEffect/BlinkEffect.cs
+++ Plugins/Effects/TTController.Plugin.BlinkEffect/BlinkEffect.cs
@@ -9,9 +9,12 @@ namespace TTController.Plugin.BlinkEffect
 {
     public class BlinkEffectConfig : EffectConfigBase
     {
-        [DefaultValue(1000)] public int On { get; private set; } = 1000;
-        [DefaultValue(1000)] public int Off { get; private set; } = 1000;
-        public List<LedColor> Colors { get; private set; } = new List<LedColor>();
+        [DefaultValue(1000)] public int OnTime { get; private set; } = 1000;
+        [DefaultValue(1000)] public int OffTime { get; private set; } = 1000;
+        [DefaultValue(null)] public LedColor? OnColor { get; private set; } = null;
+        [DefaultValue(null)] public LedColor? OffColor { get; private set; } = null;
+        [DefaultValue(null)] public List<LedColor> OnColors { get; private set; } = null;
+        [DefaultValue(null)] public List<LedColor> OffColors { get; private set; } = null;
     }
 
     public class BlinkEffect : EffectBase<BlinkEffectConfig>
@@ -25,10 +28,13 @@ namespace TTController.Plugin.BlinkEffect
 
         public override IDictionary<PortIdentifier, List<LedColor>> GenerateColors(List<PortIdentifier> ports, ICacheProvider cache)
         {
+            List<LedColor> Clone(LedColor? color, int count)
+                => color != null ? Enumerable.Repeat(color.Value, count).ToList() : null;
+
             var current = Environment.TickCount;
             var diff = current - _ticks;
 
-            if ((_state && diff > Config.On) || (!_state && diff > Config.Off))
+            if ((_state && diff > Config.OnTime) || (!_state && diff > Config.OffTime))
             {
                 _ticks = current;
                 _state = !_state;
@@ -41,15 +47,11 @@ namespace TTController.Plugin.BlinkEffect
                 if (config == null)
                     continue;
 
+                var ledCount = cache.GetDeviceConfig(port).LedCount;
                 if (_state)
-                {
-                    result.Add(port, Config.Colors.ToList());
-                }
+                    result.Add(port, Clone(Config.OnColor, ledCount) ?? Config.OnColors ?? Clone(new LedColor(0, 0, 0), ledCount));
                 else
-                {
-                    var offColor = new LedColor(0, 0, 0);
-                    result.Add(port, Enumerable.Range(0, cache.GetDeviceConfig(port).LedCount).Select(_ => offColor).ToList());
-                }
+                    result.Add(port, Clone(Config.OffColor, ledCount) ?? Config.OffColors ?? Clone(new LedColor(0, 0, 0), ledCount));
             }
 
             return result;
