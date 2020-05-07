cc91eae70647b2f6a163d122e37776c4a75753f4
MoshiMoshi0 <kamil.pietka@yahoo.pl>
2020-05-07 23:14:52 +0200

    Fix build

--- Source/TTController.Service/Utils/PluginStore.cs
+++ Source/TTController.Service/Utils/PluginStore.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using TTController.Common;
 using TTController.Common.Plugin;
 using TTController.Service.Config;
 
