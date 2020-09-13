4af73d4f5a81e9f267ae7c60117a947ba84da273
MoshiMoshi0 <kamil.pietka@yahoo.pl>
2020-09-13 20:17:01 +0200

    Add IpcEffect and IpcSpeedController

---
+++ Source/TTController.Common/Plugin/Effect/IpcEffectBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace TTController.Common.Plugin
+{
+    public abstract class IpcEffectBase<T> : EffectBase<T>, IIpcClient where T : EffectConfigBase
+    {
+        private Task _receiveTask;
+        private CancellationTokenSource _cancellationSource;
+
+        public abstract string IpcName { get; }
+        public Channel<string> SendChannel { get; private set; }
+        public Channel<string> ReceiveChannel { get; private set; }
+
+        protected IpcEffectBase(T config) : base(config)
+        {
+            SendChannel = null;
+            ReceiveChannel = Channel.CreateBounded<string>(8);
+
+            _cancellationSource = new CancellationTokenSource();
+            _receiveTask = Task.Factory.StartNew(() => ReceiveAsync(_cancellationSource.Token), _cancellationSource.Token);
+        }
+
+        protected abstract void OnDataReceived(string data);
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            _cancellationSource.Cancel();
+            _receiveTask.Wait();
+        }
+
+        private async void ReceiveAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    var result = await ReceiveChannel.Reader.ReadAsync(cancellationToken);
+                    OnDataReceived(result);
+                }
+            }
+            catch (OperationCanceledException) { }
+        }
+    }
+}
